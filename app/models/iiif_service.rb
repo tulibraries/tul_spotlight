f1c6086e9b7550934c01e4a3627ec3a3bd0f11a9
Brian Hoffman <brian.hoffman0003@temple.edu>
2021-01-19 12:40:43 -0500

    customize IIIFService class to support paginated CDM manifests

---
+++ app/models/iiif_service.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class IiifService < Spotlight::Resources::IiifService
+
+  def manifests
+    @manifests ||= if manifest?
+                     [create_iiif_manifest(object)]
+                   elsif object.respond_to? :manifests
+                     result = object.manifests.map { |manifest|
+                       create_iiif_manifest(manifest)
+                     }
+                     if collection?
+                       result << create_iiif_manifest(object)
+                     end
+                     result
+                   else
+                     build_collection_manifest.to_a
+                   end
+  end
+
+  def self.recursive_manifests(thing, &block)
+    return to_enum(:recursive_manifests, thing) unless block_given?
+    thing.manifests.each(&block)
+
+    thing.collections.each do |collection|
+      recursive_manifests(collection, &block)
+    end
+
+    thing.with_each_page do |page_thing|
+      recursive_manifests(page_thing, &block)
+    end
+  end
+
+  def with_each_page
+    return unless collection?
+    %w(first next).each do |pointer|
+      next_page_url = object.to_ordered_hash.dig(pointer, "@id")
+      yield self.class.new(next_page_url) if next_page_url
+    end
+  end
+end
